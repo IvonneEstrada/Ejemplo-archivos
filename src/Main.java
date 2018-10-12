6866fcfc6ce92e0968f634952a2495e177386c2c
IvonneEstrada <ivonne_montserrat@outlook.com>
2018-10-12 04:56:14 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+	    File entrada = new File("entrada.txt");
+        Scanner escaner = new Scanner(entrada);
+        double total = 0;
+        while(escaner.hasNext())
+        {
+            total = total + escaner.nextDouble();
+        }
+        System.out.println(total);
+    }
+}
